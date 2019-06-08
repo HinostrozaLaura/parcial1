a2441e846b043e3d12ceb62daf835081e7cf957d
HinostrozaLaura <the_ktz_15@hotmail.com>
2019-06-08 18:44:32 -0500

    agreguetipos de helado

--- src/componentes/detallehelados.js
+++ src/componentes/detallehelados.js
@@ -1,8 +1,16 @@
 import React from "react";
-const DETALLEHELADO =()=>(
+const DETALLEHELADO =({nombre,des,costo})=>{
+    return(
+        <div className="Helados">
+            <h3>Nombre del helado : {nombre}</h3>
+            <hr/>
+            <p className="Comentarios">Comentarios : {des}</p>
+            <hr/>
+            <span>Costo del Helado {costo} soles</span>
+            <br/>
+            <hr/>
 
-    <div>
-descripcion del proyecto
-    </div>
-)
+        </div>
+    )
+}
  export default DETALLEHELADO;
