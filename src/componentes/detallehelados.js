2c0fdac098347f35302fceb4bd63a39c1d519d73
HinostrozaLaura <the_ktz_15@hotmail.com>
2019-06-08 19:06:12 -0500

    finalizacion y llenado de imagenes

--- src/componentes/detallehelados.js
+++ src/componentes/detallehelados.js
@@ -1,8 +1,9 @@
 import React from "react";
-const DETALLEHELADO =({nombre,des,costo})=>{
+const DETALLEHELADO =({nombre,des,costo,imge})=>{
     return(
         <div className="Helados">
             <h3>Nombre del helado : {nombre}</h3>
+            <img style ={{with:100 ,height:200 } }src={imge}></img>
             <hr/>
             <p className="Comentarios">Comentarios : {des}</p>
             <hr/>
--- src/componentes/listadodehelados.js
+++ src/componentes/listadodehelados.js
@@ -6,16 +6,19 @@ const LISTADODEHELADOS =()=>(
 <div >
 
     <div>
-       <DETALLEHELADO  nombre="Chocolate con almendras" des="Para mí el helado de chocolate con almendras tendría que estar entre los primeros lugares de la lista. Pero por ahora me  conformo con saber que está entre los 13 más populares del mundo." costo={25}></DETALLEHELADO>
-       <DETALLEHELADO nombre=" Menta" des="¿Qué decir del helado de menta? Personalmente no me gusta, pero creo que es uno de esos sabores que los amas o los odias; no existe el punto medio." costo={15}></DETALLEHELADO>
-       <DETALLEHELADO nombre="Marmolado de vainilla con salsa de chocolate" des="Quizás uno de los favoritos de los niños pequeños y uno de esos que nos trae recuerdos de la infancia, el helado marmolado de vainilla con salsa de chocolate no podía faltar en esta lista." costo={21}></DETALLEHELADO>
-       <DETALLEHELADO nombre="Crema con galletas Oreo" des="Si el helado de crema es delicioso, solo puede mejorar cuando le agregas galletas Oreo, y así es como surge el delicioso helado de crema con galletas Oreo." costo={30}></DETALLEHELADO>
-       <DETALLEHELADO nombre=" Chocolate amargo" des="Conocido también como chocolate holandés, o chocolate fuerte, o doble chocolate, el helado de chocolate amargo es un clásico de los amantes del chocolate." costo={18}></DETALLEHELADO>
-       <DETALLEHELADO nombre="Crema con chips de chocolate" des="Otro de los sabores clásicos que no podía faltar en esta lista es el helado de crema con chips de chocolate. ¡Favorito de niños y adultos por igual!" costo={26}></DETALLEHELADO>
-       <DETALLEHELADO nombre="Dulce de leche" des="El helado de dulce de leche es un clásico de América del Sur y se extiende como un sabor exótico en el resto del mundo. ¿Su sabor? Simplemente irresistible. " costo={16}></DETALLEHELADO>
+
+       <DETALLEHELADO  nombre="Chocolate con almendras" imge={require('../img/chocolatelamendra.jpg')} des="Para mí el helado de chocolate con almendras tendría que estar entre los primeros lugares de la lista. Pero por ahora me  conformo con saber que está entre los 13 más populares del mundo." costo={25}></DETALLEHELADO>
+      
+       <DETALLEHELADO nombre=" Menta" imge={require('../img/menta.jpg')} des="¿Qué decir del helado de menta? Personalmente no me gusta, pero creo que es uno de esos sabores que los amas o los odias; no existe el punto medio." costo={15}></DETALLEHELADO>
+       <DETALLEHELADO nombre="Marmolado de vainilla con salsa de chocolate" imge={require('../img/marmolilla.jpg')} des="Quizás uno de los favoritos de los niños pequeños y uno de esos que nos trae recuerdos de la infancia, el helado marmolado de vainilla con salsa de chocolate no podía faltar en esta lista." costo={21}></DETALLEHELADO>
+       <DETALLEHELADO nombre="Crema con galletas Oreo"imge={require('../img/oreo.jpg')}  des="Si el helado de crema es delicioso, solo puede mejorar cuando le agregas galletas Oreo, y así es como surge el delicioso helado de crema con galletas Oreo." costo={30}></DETALLEHELADO>
+       <DETALLEHELADO nombre=" Chocolate amargo" imge={require('../img/amargo.jpg')}  des="Conocido también como chocolate holandés, o chocolate fuerte, o doble chocolate, el helado de chocolate amargo es un clásico de los amantes del chocolate." costo={18}></DETALLEHELADO>
+       <DETALLEHELADO nombre="Crema con chips de chocolate" imge={require('../img/chocochips.jpg')} des="Otro de los sabores clásicos que no podía faltar en esta lista es el helado de crema con chips de chocolate. ¡Favorito de niños y adultos por igual!" costo={26}></DETALLEHELADO>
+       <DETALLEHELADO nombre="Dulce de leche" imge={require('../img/dulceleche.jpg')} des="El helado de dulce de leche es un clásico de América del Sur y se extiende como un sabor exótico en el resto del mundo. ¿Su sabor? Simplemente irresistible. " costo={16}></DETALLEHELADO>
        
       
     </div>
+    <br/>
     </div>
 );
 export default LISTADODEHELADOS;
